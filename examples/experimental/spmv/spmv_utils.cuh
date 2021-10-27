7b5e32c803c38f6a2bf76e4f8ef04ebd4a4fc475
Jonathan Wapman <jdwapman@ucdavis.edu>
2021-10-27 15:54:14 -0700

    Add some useful helper functions

---
+++ examples/experimental/spmv/spmv_utils.cuh
@@ -0,0 +1,25 @@
+#pragma once
+
+#define CHECK_CUDA(func)                                                   \
+  {                                                                        \
+    cudaError_t status = (func);                                           \
+    if (status != cudaSuccess) {                                           \
+      printf("CUDA API failed at line %d with error: %s (%d)\n", __LINE__, \
+             cudaGetErrorString(status), status);                          \
+      return EXIT_FAILURE;                                                 \
+    }                                                                      \
+  }
+
+template <typename vector_t>
+void display(vector_t v, std::string name, bool verbose = true) {
+  if (verbose) {
+    std::cout << name << " = [ ";
+    for (size_t i = 0; i < v.size() && (i < 40); i++)
+      std::cout << v[i] << " ";
+
+    if (v.size() >= 40) {
+      std::cout << "...";
+    }
+    std::cout << "]" << std::endl;
+  }
+}
