6501fb63dbde02da03cdc7696b043457ce08551a
Jonathan Wapman <jdwapman@ucdavis.edu>
2021-11-17 15:40:04 -0800

    Move SPMV launch into a separate function

--- examples/experimental/spmv/spmv.cu
+++ examples/experimental/spmv/spmv.cu
@@ -6,6 +6,66 @@ using namespace gunrock;
 using namespace experimental;
 using namespace memory;
 
+enum SPMV_t { MGPU, CUB, CUSPARSE, TILED };
+enum LB_t { THREAD_PER_ROW, WARP_PER_ROW, BLOCK_PER_ROW, MERGE_PATH };
+
+template <>
+double run_test() {
+  // Reset the output vector
+  thrust::fill(dout.begin(), dout.end(), 0);
+
+  double elapsed_time = 0;
+
+  //   Run on appropriate GPU implementation
+  if (spmv_impl == MGPU) {
+    elapsed_time = spmv_mgpu(sparse_matrix, din, dout);
+  } else if (spmv_impl == CUB) {
+    // elapsed_time = spmv_cub(sparse_matrix, din, dout);
+  } else if (spmv_impl == CUSPARSE) {
+    elapsed_time = spmv_cusparse(sparse_matrix, din, dout);
+  } else if (spmv_impl == TILED) {
+    elapsed_time = spmv_tiled(sparse_matrix, din, dout, debug);
+  } else {
+    std::cout << "Unsupported SPMV implementation" << std::endl;
+  }
+
+  printf("GPU finished in %lf ms\n", elapsed_time);
+
+  //   Copy results to CPU
+  if (check) {
+    thrust::host_vector<float> h_output = dout;
+
+    // Run on CPU
+    thrust::host_vector<float> cpu_ref(sparse_matrix.num_rows);
+    cpu_spmv(sparse_matrix, hin, cpu_ref);
+
+    for (index_t row = 0; row < sparse_matrix.num_rows; row++) {
+      cpu_ref[row] = 0.0;
+      // Loop over all the non-zeroes within A's row
+      for (auto k = sparse_matrix.row_offsets[row];
+           k < sparse_matrix.row_offsets[row + 1]; ++k)
+        cpu_ref[row] +=
+            sparse_matrix.nonzero_vals[k] * hin[sparse_matrix.col_idx[k]];
+    }
+
+    util::display(hin, "cpu_in");
+    util::display(din, "gpu_in");
+    util::display(cpu_ref, "cpu_out");
+    util::display(dout, "gpu_out");
+
+    // Validate
+    bool passed = validate(h_output, cpu_ref);
+    if (passed) {
+      std::cout << "Validation Successful" << std::endl;
+      return elapsed_time;
+    } else {
+      std::cout << "Validation Failed" << std::endl;
+      return -1;
+    }
+  }
+  return elapsed_time;
+}
+
 void test_spmv(int num_arguments, char** argument_array) {
   if (num_arguments != 2) {
     std::cerr << "usage: ./bin/<program-name> filename.mtx" << std::endl;
