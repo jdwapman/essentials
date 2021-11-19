6fb65d2a1cac9a9039311b0aa87bfbea7bdfcee7
Jonathan Wapman <jdwapman@ucdavis.edu>
2021-11-18 16:06:06 -0800

    Move spmv launcher into a separate function

--- examples/experimental/spmv/spmv.cu
+++ examples/experimental/spmv/spmv.cu
@@ -9,61 +9,69 @@ using namespace memory;
 enum SPMV_t { MGPU, CUB, CUSPARSE, TILED };
 enum LB_t { THREAD_PER_ROW, WARP_PER_ROW, BLOCK_PER_ROW, MERGE_PATH };
 
-// double run_test() {
-//   // Reset the output vector
-//   thrust::fill(dout.begin(), dout.end(), 0);
+template <typename csr_t, typename vector_t>
+double test_spmv(SPMV_t spmv_impl,
+                 csr_t& sparse_matrix,
+                 vector_t& d_input,
+                 vector_t& d_output,
+                 bool cpu_verify,
+                 bool debug) {
+  // Reset the output vector
+  thrust::fill(d_output.begin(), d_output.end(), 0);
 
-//   double elapsed_time = 0;
+  double elapsed_time = 0;
 
-//   //   Run on appropriate GPU implementation
-//   if (spmv_impl == MGPU) {
-//     elapsed_time = spmv_mgpu(sparse_matrix, din, dout);
-//   } else if (spmv_impl == CUB) {
-//     // elapsed_time = spmv_cub(sparse_matrix, din, dout);
-//   } else if (spmv_impl == CUSPARSE) {
-//     elapsed_time = spmv_cusparse(sparse_matrix, din, dout);
-//   } else if (spmv_impl == TILED) {
-//     elapsed_time = spmv_tiled(sparse_matrix, din, dout, debug);
-//   } else {
-//     std::cout << "Unsupported SPMV implementation" << std::endl;
-//   }
+  //   Run on appropriate GPU implementation
+  if (spmv_impl == MGPU) {
+    // elapsed_time = spmv_mgpu(sparse_matrix, d_input, d_output);
+  } else if (spmv_impl == CUB) {
+    // elapsed_time = spmv_cub(sparse_matrix, d_input, d_output);
+  } else if (spmv_impl == CUSPARSE) {
+    elapsed_time = spmv_cusparse(sparse_matrix, d_input, d_output);
+  } else if (spmv_impl == TILED) {
+    // elapsed_time = spmv_tiled(sparse_matrix, d_input, d_output);
+  } else {
+    std::cout << "Unsupported SPMV implementation" << std::endl;
+  }
 
-//   printf("GPU finished in %lf ms\n", elapsed_time);
+  if (debug)
+    printf("GPU finished in %lf ms\n", elapsed_time);
 
-//   //   Copy results to CPU
-//   if (check) {
-//     thrust::host_vector<float> h_output = dout;
+  //   Copy results to CPU
+  if (cpu_verify) {
+    thrust::host_vector<float> h_output = d_output;
+    thrust::host_vector<float> h_input = d_input;
 
-//     // Run on CPU
-//     thrust::host_vector<float> cpu_ref(sparse_matrix.num_rows);
-//     cpu_spmv(sparse_matrix, hin, cpu_ref);
+    // Run on CPU
+    thrust::host_vector<float> cpu_ref(sparse_matrix.number_of_rows);
+    cpu_spmv(sparse_matrix, h_input, cpu_ref);
 
-//     for (index_t row = 0; row < sparse_matrix.num_rows; row++) {
-//       cpu_ref[row] = 0.0;
-//       // Loop over all the non-zeroes within A's row
-//       for (auto k = sparse_matrix.row_offsets[row];
-//            k < sparse_matrix.row_offsets[row + 1]; ++k)
-//         cpu_ref[row] +=
-//             sparse_matrix.nonzero_vals[k] * hin[sparse_matrix.col_idx[k]];
-//     }
+    if(debug) {
+      display(d_input, "d_input");
+      display(d_output, "d_output");
+      display(cpu_ref, "cpu_ref");
+    }
 
-//     util::display(hin, "cpu_in");
-//     util::display(din, "gpu_in");
-//     util::display(cpu_ref, "cpu_out");
-//     util::display(dout, "gpu_out");
+    // Validate
+    int num_errors = check_spmv(cpu_ref, h_output);
 
-//     // Validate
-//     bool passed = validate(h_output, cpu_ref);
-//     if (passed) {
-//       std::cout << "Validation Successful" << std::endl;
-//       return elapsed_time;
-//     } else {
-//       std::cout << "Validation Failed" << std::endl;
-//       return -1;
-//     }
-//   }
-//   return elapsed_time;
-// }
+    // Print the number of errors
+    if(debug)
+    printf("Errors: %d\n", num_errors);
+
+    if (!num_errors) {
+      if (debug)
+        std::cout << "Validation Successful" << std::endl;
+      return elapsed_time;
+    } else {
+      if (debug)
+        std::cout << "Validation Failed" << std::endl;
+      return -1;
+    }
+  }
+
+  return elapsed_time;
+}
 
 void test_spmv(int num_arguments, char** argument_array) {
   if (num_arguments != 2) {
@@ -96,19 +104,6 @@ void test_spmv(int num_arguments, char** argument_array) {
     exit(1);
   }
 
-  // --
-  // Build graph + metadata
-
-  // auto sparse_matrix = graph::build::from_csr<memory_space_t::device,
-  //                                             graph::view_t::csr>(
-  //     csr.number_of_rows,               // rows
-  //     csr.number_of_columns,            // columns
-  //     csr.number_of_nonzeros,           // nonzeros
-  //     csr.row_offsets.data().get(),     // row_offsets
-  //     csr.column_indices.data().get(),  // column_indices
-  //     csr.nonzero_values.data().get()   // values
-  // );
-
   thrust::host_vector<nonzero_t> x_host(csr.number_of_columns);
 
   srand(0);
@@ -118,41 +113,17 @@ void test_spmv(int num_arguments, char** argument_array) {
   thrust::device_vector<nonzero_t> x_device = x_host;
   thrust::device_vector<nonzero_t> y_device(csr.number_of_rows);
 
-  thrust::host_vector<nonzero_t> y_ref_host(csr.number_of_rows);
-
   // --
   // Run the algorithm
 
-  bool verify = true;
+  bool cpu_verify = true;
+  bool debug = true;
 
-  double elapsed_cusparse = spmv_cusparse(csr, x_device, y_device);
+  double elapsed_cusparse =
+      test_spmv(CUSPARSE, csr, x_device, y_device, cpu_verify, debug);
 
-  double elapsed_tiled = 0;
-  // double elapsed_tiled = run_tiled(sparse_matrix, x_device, y_device);
-
-  int num_errors_cusparse = 0;
-  if (verify) {
-    // Get the output from the device
-    thrust::host_vector<nonzero_t> y_host = y_device;
-
-    // Compute the reference solution
-    cpu_spmv(csr, x_host, y_ref_host);
-
-    // Print the cuSparse computed vector
-    display(y_host, "y_cusparse");
-
-    // Print the reference computed vector
-    display(y_ref_host, "y_ref_host");
-
-    num_errors_cusparse = check_spmv(y_ref_host, y_host);
-  }
-
-  printf("%s,%d,%d,%d,%f,%f\n", filename.c_str(), csr.number_of_rows,
-         csr.number_of_columns, csr.number_of_nonzeros, elapsed_cusparse,
-         elapsed_tiled);
-
-  // Print the number of errors
-  printf("Errors: %d\n", num_errors_cusparse);
+  printf("%s,%d,%d,%d,%f\n", filename.c_str(), csr.number_of_rows,
+         csr.number_of_columns, csr.number_of_nonzeros, elapsed_cusparse);
 }
 
 int main(int argc, char** argv) {
