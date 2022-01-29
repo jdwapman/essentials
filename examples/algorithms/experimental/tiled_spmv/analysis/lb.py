24dd4f6c10306faf9489aad2d4d0724d269ff87d
Jonathan Wapman <jdwapman@ucdavis.edu>
2022-01-29 15:20:32 -0800

    Add some load imbalance statistics

--- examples/algorithms/experimental/tiled_spmv/analysis/lb.py
+++ examples/algorithms/experimental/tiled_spmv/analysis/lb.py
@@ -2,6 +2,7 @@ from math import ceil
 from scipy.sparse import csr_matrix
 import scipy.io as sio
 import sys
+import numpy as np
 
 tile_size = 10
 num_SMs = 2
@@ -24,10 +25,34 @@ num_col_tiles = ceil(csr_mat.shape[1] / tile_size)
 print("\t- Number of row tiles:", num_row_tiles)
 print("\t- Number of column tiles:", num_col_tiles)
 
-iter = 0
-for row_tile_idx in range(0, num_row_tiles, num_SMs):
-    for col_tile_idx in range(0, num_col_tiles):
-        print("Iteration: ", iter)
-        iter += 1
+for col_tile_idx in range(0, num_col_tiles):
+    print("Column tile:", col_tile_idx)
+    for row_tile_idx in range(0, num_row_tiles, num_SMs):
+        print("\tRow tile:", row_tile_idx)
+
+        # Create a numpy array equal to the number of SMs
+        # This will be used to store the number of non-zero elements
+        # in each SM
+        sm_nnz = [0] * num_SMs
         for sm_idx in range(0, num_SMs):
-            print("\t- SM %d: (%d,%d)" % (sm_idx, row_tile_idx, col_tile_idx * num_SMs + sm_idx))
+            print("\t- SM %d: (%d,%d)" %
+                  (sm_idx, row_tile_idx + sm_idx, col_tile_idx))
+
+            # Get the tile corresponding to this SM
+            row_start = (row_tile_idx + sm_idx) * tile_size
+            row_end = min(row_start + tile_size, csr_mat.shape[0])
+            col_start = col_tile_idx * tile_size
+            col_end = min(col_start + tile_size, csr_mat.shape[1])
+            tile = csr_mat[row_start:row_end, col_start:col_end]
+            print("\t\t- Row range:", row_start, row_end)
+            print("\t\t- Column range:", col_start, col_end)
+            print("\t\t- Nonzero elements in tile:", tile.nnz)
+            sm_nnz[sm_idx] = tile.nnz
+
+        # Compute the load imbalance using the coefficient of variation
+        # of the number of non-zero elements in each SM. Use numpy functions
+        sm_nnz_mean = np.mean(sm_nnz)
+        sm_nnz_std = np.std(sm_nnz)
+        print("\t- Mean number of non-zero elements per SM:", sm_nnz_mean)
+        print("\t- Standard deviation of non-zero elements per SM:", sm_nnz_std)
+        print("\t- Load imbalance:", sm_nnz_std / sm_nnz_mean)
