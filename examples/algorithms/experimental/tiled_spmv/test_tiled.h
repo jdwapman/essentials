ee5fee4cf1a8b2bea23e49ff382d7fe069c200b2
Jonathan Wapman <jdwapman@ucdavis.edu>
2022-02-26 18:24:11 -0800

    Setup tile processing stubs

--- examples/algorithms/experimental/tiled_spmv/test_tiled.h
+++ examples/algorithms/experimental/tiled_spmv/test_tiled.h
@@ -44,316 +44,124 @@ class TileIterator {
   __device__ TileIterator(const graph_t _graph,
                           const vector_t* _input,
                           vector_t* _output,
-                          const size_t _rows_per_block_tile,
+                          const size_t _tile_row_size,
                           const size_t _tile_col_size,
                           shmem_t* _shmem)
       : graph(_graph),
         input(_input),
         output(_output),
-        rows_per_block_tile(_rows_per_block_tile),
+        tile_row_size(_tile_row_size),
         tile_col_size(_tile_col_size),
         shmem(_shmem) {
-    // (size_t)(_rows_per_block_tile *2* sizeof(index_t)));
-
-    // rows_per_block_tile = _rows_per_block_tile;
-    // rows_per_gpu_tile = _rows_per_block_tile * gridDim.x;
-
-    // cur_row_tile_idx = 0;
-    // cur_col_tile_idx = 0;
-
-    // local_row_offsets_start = shmem;
-    // local_row_offsets_end = &local_row_offsets_start[rows_per_block_tile];
-
-    // lb_stats = _lb_stats;
+    cur_tile_col_idx = 0;
+    cur_tile_row_idx = blockIdx.x;
   }
 
-  //   __device__ __forceinline__ index_t
-  //   row_elems_in_tile(const index_t& global_row_idx,
-  //                     const index_t& block_row_idx,
-  //                     const index_t& tile_boundary) {
-  //     assert(store_end_offsets_in_shmem);
+  __device__ __forceinline__ bool all_columns_finished() { return false; }
 
-  //     index_t total_remaining_nonzeros = 0;
+  __device__ __forceinline__ void load_block_row_tile_metadata() {}
 
-  //     index_t begin = local_row_offsets_start[block_row_idx];
-  //     index_t end = local_row_offsets_end[block_row_idx];
+  __device__ __forceinline__ void lb_block_row_tile() {}
 
-  //     while (begin < end) {
-  //       index_t mid = floor((begin + end) / 2);
+  __device__ __forceinline__ void process_block_row_tile() {}
 
-  //       index_t col_key = col_idx[mid];
+  __device__ __forceinline__ void unload_block_row_tile_metadata() {}
 
-  //       if (col_key > tile_boundary) {
-  //         end = mid;
-  //       } else {
-  //         begin = mid + 1;
-  //       }
-  //     }
+  __device__ __forceinline__ void get_next_block_row_tile() {
+    // Atomically increment the current tile row index
+    // Note that this needs to be to a GLOBAL variable so that all blocks
+    // can see it.
+  }
 
-  //     index_t actual_end = end - 1;
+  __device__ __forceinline__ void process_gpu_row_tile() {
+    // Within a block, process the current tile
 
-  //     return (actual_end - local_row_offsets_start[block_row_idx]);
-  //   }
+    // Iterate over the tiles as long as it's in bounds.
 
-  //   __device__ __forceinline__ bool all_tiles_finished() {
-  //     // How many evenly-sized tiles are there?
-  //     index_t number_of_gpu_tiles_in_matrix = (num_rows / rows_per_gpu_tile);
+    // The GPU has its tile row index and col index. Need to first load in
+    // metadata, then do the load balancing, then do the computation, then
+    // unload any metadata we need to save to global mem for the next time we
+    // come back to this row. Finally, need to increment an atomic and go on to
+    // the next row tile if there are more to process.
 
-  //     // Remainder tile
-  //     if (num_rows % rows_per_gpu_tile)
-  //       number_of_gpu_tiles_in_matrix++;
+    // Then do a grid-wide synchronization.
 
-  //     // cur_row_tile_idx incremented only after the primary tile is finished
-  //     // (with all its member column tiles)
-  //     if (cur_row_tile_idx >= number_of_gpu_tiles_in_matrix) {
-  //       return true;
-  //     } else {
-  //       return false;
-  //     }
-  //   }
+    auto num_row_tiles = graph.get_number_of_rows() / tile_row_size;
 
-  //   __device__ __forceinline__ void load_primary_tile() {
-  //     if (blockIdx.x == 0 && threadIdx.x == 0 && debug) {
-  //       printf("Loading Metadata for tile (%d,...) into shmem\n",
-  //              cur_row_tile_idx);
-  //     }
-  //     // Need to simultaneously keep track of the current row in the tile as
-  //     well
-  //         // as the row index in the global coordinates
-
-  //         int cur_row_in_gpu_tile =
-  //             blockIdx.x * rows_per_block_tile + threadIdx.x;
-  //     int cur_row_in_matrix =
-  //         tile2global(cur_row_in_gpu_tile, cur_row_tile_idx,
-  //         rows_per_gpu_tile);
-
-  //     int cur_row_in_block_tile = threadIdx.x;
-
-  //     int stride = blockDim.x;
-
-  //     // Iterate over all rows in the current tile
-  //     for (; cur_row_in_matrix < num_rows &&
-  //            cur_row_in_block_tile < rows_per_block_tile;
-  //          cur_row_in_matrix += stride, cur_row_in_block_tile += stride,
-  //          cur_row_in_gpu_tile += stride) {
-  //       local_row_offsets_start[cur_row_in_block_tile] =
-  //           row_offsets[cur_row_in_matrix];
-
-  //       if (store_end_offsets_in_shmem) {
-  //         local_row_offsets_end[cur_row_in_block_tile] =
-  //             row_offsets[cur_row_in_matrix + 1];
-  //       }
-  //       // printf(
-  //       //     "Block %d Loading matrix row %d block tile idx %d gpu tile idx
-  //       % d "
-  //       //     "offset %d\n",
-  //       //     blockIdx.x, cur_row_in_matrix, cur_row_in_block_tile,
-  //       //     cur_row_in_gpu_tile,
-  //       local_row_offsets[cur_row_in_block_tile]);
-  //     }
-
-  //     __syncthreads();
-  //   }
-
-  //   __device__ __forceinline__ void load_secondary_tile(){};
-
-  //   __device__ __forceinline__ void evict_primary_tile() {}
-
-  //   __device__ __forceinline__ void evict_secondary_tile(){
-  //       // In the src-first implementation, there is nothing to do for this
-  //       function
-  //       // except maybe resetting the L2 cache
-  //   }
-
-  //   __device__ __forceinline__ bool primary_tile_finished() {
-  //     // How many evenly-sized tiles are there?
-  //     index_t number_of_tiles_in_matrix = (num_cols / tile_col_size);
-
-  //     // Remainder tile
-  //     if (num_cols % tile_col_size)
-  //       number_of_tiles_in_matrix++;
-
-  //     if (cur_col_tile_idx >= number_of_tiles_in_matrix) {
-  //       return true;
-  //     } else {
-  //       return false;
-  //     }
-  //   };
-
-    __device__ __forceinline__ void process_all_tiles() {
-      // while (!all_tiles_finished()) {
-      //   load_primary_tile();
-      //   process_primary_tile();
-      //   // evict_primary_tile();
-      // }
+    // Handle the remainder
+    if (graph.get_number_of_rows() % tile_row_size != 0) {
+      num_row_tiles++;
     }
 
-  //   __device__ __forceinline__ void process_primary_tile() {
-  //     // if (blockIdx.x == 0 && threadIdx.x == 0) {
-  //     //   printf("Processing Tile (%d,...)\n", cur_row_tile_idx);
-  //     // }
-  //     while (!primary_tile_finished()) {
-  //       // load_secondary_tile();
-  //       process_secondary_tile();
-  //       // evict_secondary_tile();
-  //     }
+    // All blocks iterate over the row tiles
+    while (cur_tile_row_idx < num_row_tiles) {
+      // Load metadata
+      load_block_row_tile_metadata();
 
-  //     cur_row_tile_idx++;
-  //     cur_col_tile_idx = 0;
-  //   }
+      // Load balancing
+      lb_block_row_tile();
 
-  //   __device__ __forceinline__ void lb_warp_per_row() {}
+      // Compute
+      process_block_row_tile();
 
-  //   __device__ __forceinline__ void lb_thread_per_row() {
-  //     cg::grid_group grid = cg::this_grid();
+      // Unload metadata
+      unload_block_row_tile_metadata();
 
-  //     if (debug) {
-  //       lb_stats[blockIdx.x] = 0;
-  //       __syncthreads();
-  //     }
+      // Increment atomic
+      get_next_block_row_tile();
+    }
 
-  //     int cur_row_in_gpu_tile = blockIdx.x * rows_per_block_tile +
-  //     threadIdx.x; int cur_row_in_matrix =
-  //         tile2global(cur_row_in_gpu_tile, cur_row_tile_idx,
-  //         rows_per_gpu_tile);
+    // Sync
+    cg::grid_group grid = cg::this_grid();
+    grid.sync();
+  }
 
-  //     int cur_row_in_block_tile = threadIdx.x;
+  __device__ __forceinline__ void get_next_gpu_row_tile() {
+    // Reset the tile metadata
+    cur_tile_row_idx = blockIdx.x;
+    cur_tile_col_idx += 1;
 
-  //     int stride = blockDim.x;
+    // TODO do something to reset the caching here?
+  }
 
-  //     // End of the col tile boundary
-  //     index_t tile_boundary =
-  //         min(num_cols, (cur_col_tile_idx + 1) * tile_col_size);
-
-  //     // Iterate over all rows in the current tile
-  //     for (; cur_row_in_matrix < num_rows &&
-  //            cur_row_in_block_tile < rows_per_block_tile;
-  //          cur_row_in_matrix += stride, cur_row_in_block_tile += stride,
-  //          cur_row_in_gpu_tile += stride) {
-  //       // Process a row
-
-  //       printf(
-  //           "Block %d, Thread %d, Global Row %d, Local Row %d, Tile Nonzeros
-  //           "
-  //           "%d\n",
-  //           blockIdx.x, threadIdx.x, cur_row_in_matrix,
-  //           cur_row_in_block_tile, row_elems_in_tile(cur_row_in_matrix,
-  //           cur_row_in_block_tile,
-  //                             tile_boundary));
-
-  //       value_t sum = 0.0;
-  //       index_t offset = local_row_offsets_start[cur_row_in_block_tile];
-
-  //       index_t max_offset;
-
-  //       if (store_end_offsets_in_shmem) {
-  //         max_offset = local_row_offsets_end[cur_row_in_block_tile];
-  //       } else {
-  //         max_offset = row_offsets[cur_row_in_block_tile + 1];
-  //       }
-
-  //       while (true) {
-  //         if (offset >= max_offset)
-  //           break;
-
-  //         index_t col = col_idx[offset];
-
-  //         if (col >= tile_boundary) {
-  //           // printf("Col %d greater than boundary %d\n", col,
-  //           tile_boundary); break;
-  //         } else {
-  //           // printf("Processing col %d\n", col);
-  //         }
-  //         // atomicAdd(&block_nonzeros, 1);
-  //         sum += nonzeros[offset] * input[col];
-
-  //         if (debug)
-  //           atomicAdd(&lb_stats[blockIdx.x], 1);
-
-  //         offset++;
-  //       }
-
-  //       // Finished with the row
-
-  //       // Save the offset for the next iteration
-  //       local_row_offsets_start[cur_row_in_block_tile] = offset;
-  //       if (sum != 0) {
-  //         output[cur_row_in_matrix] += sum;
-  //       }
-  //     }
-
-  //     // Must sync at the end of the tile to preserve cache reuse
-
-  //     grid.sync();
-
-  //     if (threadIdx.x == 0 && debug) {
-  //       printf("Tile (%d,%d) block %d has %d nonzeros\n", cur_row_tile_idx,
-  //              cur_col_tile_idx, blockIdx.x, lb_stats[blockIdx.x]);
-  //     }
-
-  //     cur_col_tile_idx++;
-  //   }
-
-  //   __device__ __forceinline__ void process_secondary_tile() {
-  //     // if (blockIdx.x == 0 && threadIdx.x == 0) {
-  //     //   printf("Processing Tile (%d,%d)\n", cur_row_tile_idx,
-  //     //   cur_col_tile_idx);
-  //     // }
-
-  //     lb_thread_per_row();
-  //     // lb_warp_per_row();
-  //   }
-
-  //  private:
-  //   // SPMV operator properties
-  //   const index_t num_rows;
-  //   const index_t num_cols;
-  //   const index_t num_nonzeros;
-  //   const index_t* row_offsets;
-  //   const index_t* col_idx;
-  //   const value_t* nonzeros;
-  //   const value_t* input;
-  //   value_t* output;
-
-  //   // Tiling metadata
-  //   index_t rows_per_block_tile;
-  //   index_t rows_per_gpu_tile;
-  //   index_t tile_col_size;
-
-  //   index_t cur_row_tile_idx;
-  //   index_t cur_col_tile_idx;
-
-  //   // shmem
-  //   index_t* local_row_offsets_start;
-  //   index_t* local_row_offsets_end;
-
-  //   index_t* lb_stats;
-
-  //   const bool store_end_offsets_in_shmem;
-
-  //   const bool debug;
+  __device__ __forceinline__ void process_all_tiles() {
+    while (!all_columns_finished()) {
+      process_gpu_row_tile();
+      get_next_gpu_row_tile();
+    }
+  }
 
  private:
+  // Store the inputs and outputs
   const graph_t graph;
-  const vector_t *input;
-  vector_t *output;
-  const size_t rows_per_block_tile;
+  const vector_t* input;
+  vector_t* output;
+  shmem_t* shmem;
+
+  // Tiling metadata
+  const size_t tile_row_size;
   const size_t tile_col_size;
-  shmem_t *shmem;
+
+  size_t cur_tile_row_idx;
+  size_t cur_tile_col_idx;
+
+  // shmem
+  shmem_t* local_row_offsets_start;
+  shmem_t* local_row_offsets_end;
 };
 
 template <typename graph_t, typename vector_t>
 __global__ void spmv_tiled_kernel(graph_t graph,
                                   vector_t* input,
                                   vector_t* output,
-                                  size_t rows_per_block_tile,
+                                  size_t tile_row_size,
                                   size_t tile_col_size) {
   // Store the output in shared memory
   using row_t = decltype(graph.get_row_offsets());
   extern __shared__ row_t shmem[];
 
   TileIterator<graph_t, vector_t, row_t> iterator(
-      graph, input, output, rows_per_block_tile, tile_col_size, shmem);
+      graph, input, output, tile_row_size, tile_col_size, shmem);
 
   iterator.process_all_tiles();
 
